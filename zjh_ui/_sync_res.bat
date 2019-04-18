30ce61ecd670de831aa8b0f82135fd3f66a3c646
nneesshh <nneesshh@163.com>
2019-04-18 19:54:02 +0800

    fixed ignore again

--- zjh_ui/_sync_res.bat
+++ zjh_ui/_sync_res.bat
@@ -1,2 +1,5 @@
-rmdir /s /q "..\zjh\res\"
+rmdir /s /q "..\zjh\res\lobby\csb\"
+rmdir /s /q "..\zjh\res\lobby\image\"
+rmdir /s /q "..\zjh\res\game\zjh\csb\"
+rmdir /s /q "..\zjh\res\game\zjh\image\"
 xcopy /sy .\res\*.* ..\zjh\res\
